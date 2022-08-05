c51076cb0f842e4e24145b221dac7fc6ea6e1c00
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-08-05 17:11:45 -0300

    Created from contact model.

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Contacts
+
 # Register your models here.
+
+
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'last_name',
+                    'telephone', 'favorite', 'create')
+    list_display_links = ('id', 'name',)
+    list_editable = ('favorite',)
+    list_filter = ('name', 'telephone', 'favorite',)
+    list_per_page = 30
+
+
+admin.site.register(Contacts, ContactsAdmin)
--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+class Contacts(models.Model):
+    name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30, blank=True)
+    email = models.EmailField(max_length=50, blank=True)
+    dd = models.IntegerField()
+    telephone = models.IntegerField()
+    birthday = models.DateField(blank=True)
+    favorite = models.BooleanField(default=False)
+    note = models.TextField(blank=True)
+    create = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name+" "+self.last_name
+
+    class Meta:
+        verbose_name_plural = 'Contacts'
+        verbose_name = 'Contact'
