9a13e2ead645f5a90f06dd59ea2e85b2c509cebe
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-08-06 15:35:41 -0300

    Using resolve to know which view function is being used.

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,5 +1,9 @@
+from pydoc import resolve
+
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import resolve, reverse
+
+from contacts import views
 
 
 # Create your tests here.
@@ -8,6 +12,16 @@ class ContactUrlsTest(TestCase):
         url = reverse('contacts:home')
         self.assertEqual(url, '/')
 
-    def test_if_the_detail_url_is_correct(self):
+    def test_if_the_contactDetail_url_is_correct(self):
         url = reverse('contacts:contact-detail', kwargs={'id': 1})
         self.assertEqual(url, '/contact-detail/1/')
+
+
+class ContactViewsTest(TestCase):
+    def test_contact_home_view_function_is_correct(self):
+        view = resolve(reverse('contacts:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_contact_contactDetail_view_function_is_correct(self):
+        view = resolve(reverse('contacts:contact-detail', kwargs={'id': 1}))
+        self.assertIs(view.func, views.contactDetail)
