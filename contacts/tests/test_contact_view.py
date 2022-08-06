982b81b6aa39fd0f1536e8ba079207f035dd3abe
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-08-06 17:30:12 -0300

    Test status code 200,404. test loading template and text.

--- contacts/tests/test_contact_view.py
+++ contacts/tests/test_contact_view.py
@@ -8,6 +8,33 @@ class ContactViewsTest(TestCase):
         view = resolve(reverse('contacts:home'))
         self.assertIs(view.func, views.home)
 
+    def test_contact_home_view_returns_status_code_200_ok(self):
+        response = self.client.get(reverse('contacts:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_home_view_load_correct_template(self):
+        response = self.client.get(reverse('contacts:home'))
+        self.assertTemplateUsed(response, 'contacts/pages/home.html')
+
+    def test_contact_home_template_shows_no_contact_found_if_no_contacts(self):
+        response = self.client.get(reverse('contacts:home'))
+        self.assertIn(
+            'Contacts',
+            response.content.decode('utf-8')
+        )
+
     def test_contact_contactDetail_view_function_is_correct(self):
         view = resolve(reverse('contacts:contact-detail', kwargs={'id': 1}))
         self.assertIs(view.func, views.contactDetail)
+
+    def test_contact_contactDetail_view_returns_404_is_no_contact_found(self):
+        response = self.client.get(
+            reverse('contacts:contact-detail', kwargs={'id': 1000}))
+        self.assertEqual(response.status_code, 404)
+
+    def test_contact_home_template_shows_no_contact_found_if_no_contacts_table(self):
+        response = self.client.get(reverse('contacts:home'))
+        self.assertIn(
+            'No Contacts found here',
+            response.content.decode('utf-8')
+        )
