6578166629d76b05f14caffffaee00406c21d552
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-08-06 15:19:18 -0300

    Starting Tests. Contact App Urls Tests

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class ContactUrlsTest(TestCase):
+    def test_if_the_home_url_is_correct(self):
+        url = reverse('contacts:home')
+        self.assertEqual(url, '/')
+
+    def test_if_the_detail_url_is_correct(self):
+        url = reverse('contacts:contact-detail', kwargs={'id': 1})
+        self.assertEqual(url, '/contact-detail/1/')
