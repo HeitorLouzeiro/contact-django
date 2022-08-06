0a40cf23ef3508ba1d5821936258c84612ac631a
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-08-06 15:47:01 -0300

    Separating tests in package called tests with __init__.py

---
+++ contacts/tests/test_contact_urls.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.urls import reverse
+
+# Create your tests here.
+
+
+class ContactUrlsTest(TestCase):
+    def test_if_the_home_url_is_correct(self):
+        url = reverse('contacts:home')
+        self.assertEqual(url, '/')
+
+    def test_if_the_contactDetail_url_is_correct(self):
+        url = reverse('contacts:contact-detail', kwargs={'id': 1})
+        self.assertEqual(url, '/contact-detail/1/')
--- contacts/tests/test_contact_view.py
+++ contacts/tests/test_contact_view.py
@@ -1,20 +1,6 @@
-from pydoc import resolve
-
+from contacts import views
 from django.test import TestCase
 from django.urls import resolve, reverse
-
-from contacts import views
-
-
-# Create your tests here.
-class ContactUrlsTest(TestCase):
-    def test_if_the_home_url_is_correct(self):
-        url = reverse('contacts:home')
-        self.assertEqual(url, '/')
-
-    def test_if_the_contactDetail_url_is_correct(self):
-        url = reverse('contacts:contact-detail', kwargs={'id': 1})
-        self.assertEqual(url, '/contact-detail/1/')
 
 
 class ContactViewsTest(TestCase):
